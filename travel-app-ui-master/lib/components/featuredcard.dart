559cf2973f7521714493c9410f914a2880928357
LPVS <lucaspvsantos123@gmail.com>
2023-04-14 00:23:12 -0300

    Apagar main.dart anterior
    Co-authored-by: Camila Lacerda Grandini <camilagrandinii@users.noreply.github.com>

--- travel-app-ui-master/lib/components/featuredcard.dart
+++ travel-app-ui-master/lib/components/featuredcard.dart
@@ -6,7 +6,6 @@ import 'package:travelappui/models/placesModel.dart';
 import 'package:travelappui/theme.dart';
 
 class FeaturedCard extends StatefulWidget {
-
   PlaceModel placeModel;
 
   FeaturedCard({this.placeModel});
@@ -20,13 +19,11 @@ class _FeaturedCardState extends State<FeaturedCard> {
   String title;
   bool favorite;
 
-  
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     ThemeData appTheme = Theme.of(context);
-    
+
     return Container(
         width: size.width * 0.85,
         height: max(200, size.height * 0.32),
@@ -39,7 +36,7 @@ class _FeaturedCardState extends State<FeaturedCard> {
             Container(
               height: double.maxFinite,
               width: double.maxFinite,
-              child: ClipRRect(                
+              child: ClipRRect(
                 borderRadius: BorderRadius.circular(18),
                 child: Image(
                   image: AssetImage(widget.placeModel.imgUrl),
@@ -73,9 +70,7 @@ class _FeaturedCardState extends State<FeaturedCard> {
                                 Icons.favorite_rounded,
                                 color: kAppTheme.primaryColor,
                               ),
-                              onPressed: () {
-
-                              })
+                              onPressed: () {})
                         ],
                       ),
                     ),

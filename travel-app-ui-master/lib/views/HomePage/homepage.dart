678657ac9ec0db48f219073c2244b409cf825ff2
LPVS <lucaspvsantos123@gmail.com>
2023-06-01 20:14:03 -0300

    Homepage changes

--- travel-app-ui-master/lib/views/HomePage/homepage.dart
+++ travel-app-ui-master/lib/views/HomePage/homepage.dart
@@ -1,11 +1,12 @@
-
-import 'package:fluttertoast/fluttertoast.dart';
-import '../Login/profile.dart';
+import 'dart:convert';
+import 'package:elegant_notification/elegant_notification.dart';
+import 'package:elegant_notification/resources/arrays.dart';
 import 'package:flutter/material.dart';
 import 'package:motion_tab_bar/motiontabbar.dart';
+import 'package:http/http.dart' as http;
 import '../../constants/colors.dart';
+import '../Login/profile.dart';
 import '../WeatherPrevision/lib2/mainWeather.dart';
-
 import 'home_page_content.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,37 +18,96 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentIndex = 1;
+  bool showExampleApp = false;
+  bool isAberta = false;
 
-  List<Widget> get pages => [
-        MainWeather(),
-        HomePageContentWidget(),
-        ProfilePage(),
-      ];
-      
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
 
+  void fetchData() async {
+    try {
+      var response = await http.get(Uri.parse('https://ihouse.azurewebsites.net/api/Sensor'));
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        setState(() {
+          isAberta = data[0]['isAberta'];
+        });
+        if (isAberta) {
+          showNotification();
+        }
+      }
+    } catch (error) {
+      // Ocorreu um erro na chamada à API
+      print('Erro na chamada da API: $error');
+    }
+  }
+
+  void showNotification() async {
+    await ElegantNotification.info(
+      width: 360,
+      notificationPosition: NotificationPosition.topLeft,
+      animation: AnimationType.fromTop,
+      title: Text(
+        'ALERTA',
+        style: TextStyle(color: Color.fromARGB(255, 240, 8, 8)),
+      ),
+      description: Text(
+        'Verifique suas janelas abertas',
+        style: TextStyle(color: Colors.black),
+      ),
+      onDismiss: () {
+        setState(() {
+          showExampleApp = true;
+        });
+      },
+    ).show(context);
+  }
+
+  void onTabItemSelected(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
       appBar: PreferredSize(
-      preferredSize: Size.fromHeight(kToolbarHeight),
-      child: Container(
-        color: Color.fromRGBO(2, 166, 231, 0.573),
-        alignment: Alignment.center,
-        child: Text(
-          "IHouse Status",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w900,
-            letterSpacing: 2,
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: Container(
+          color: Color.fromRGBO(2, 166, 231, 0.573),
+          alignment: Alignment.center,
+          child: Text(
+            "IHouse Status",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w900,
+              letterSpacing: 2,
+            ),
           ),
         ),
       ),
-    ),
-      body: IndexedStack(
-        index: currentIndex,
-        children: pages,
+      body: Stack(
+        children: [
+          if (isAberta && showExampleApp)
+            Container(
+              color: Color.fromARGB(255, 255, 255, 255), // Defina a cor de fundo desejada
+              child: Center(),
+            ),
+          if (!showExampleApp)
+            IndexedStack(
+              index: currentIndex,
+              children: [
+                MainWeather(),
+                HomePageContentWidget(),
+                ProfilePage(),
+              ],
+            ),
+        ],
       ),
       bottomNavigationBar: MotionTabBar(
         tabOneName: "Previsão do Tempo",
@@ -59,9 +119,8 @@ class _HomePageState extends State<HomePage> {
         tabIconColor: kAppBarBackground,
         tabSelectedColor: kOnAccentColor,
         textStyle: TextStyle(color: kOnAccentColor),
+        onTabItemSelected: onTabItemSelected,
       ),
     );
   }
 }
-
-
